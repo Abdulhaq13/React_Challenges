d866e627200f0e4867058fa867804f633b7c74bc
Syed Abdul Haq Shuttari <syedabdulhaqshuttari@gmail.com>
2026-03-21 21:38:26 +0530

    added tip calculator

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import Exercise5 from "./exercises/Exercise5_AccordianComponent/Exercise5-V1";
+import App from "./exercises/06.TipCalculator/App.js";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Exercise5 />
+    <App />
   </StrictMode>,
 );
